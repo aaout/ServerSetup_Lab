bfe6885300de20e2d39dbf32181862317474c240
Asuke <62407988+aaout@users.noreply.github.com>
2022-12-01 00:58:45 +0900

    Update check_env.py

--- src/check_env.py
+++ src/check_env.py
@@ -1,18 +1,20 @@
 import torch
+import tensorflow as tf
 import matplotlib as plt
 import pandas as pd
 import cv2 as cv
 
-if __name__ == '__main__':
-    print('GPU environment')
-    print(f'is_available: {torch.cuda.is_available()} ')
-    print(f'device_count: {torch.cuda.device_count()} ')
-    print(f'GPU name: {torch.cuda.get_device_name()} ')
-    print(f'device_capability: {torch.cuda.get_device_capability()} ')
+if __name__ == "__main__":
+    print("GPU environment")
+    print(f"is_available: {torch.cuda.is_available()} ")
+    print(f"device_count: {torch.cuda.device_count()} ")
+    print(f"GPU name: {torch.cuda.get_device_name()} ")
+    print(f"device_capability: {torch.cuda.get_device_capability()} ")
 
-    print('')
-    print('Pachage')
-    print(f'PyTorch: {torch.__version__} ')
-    print(f'Pandas: {pd.__version__} ')
-    print(f'OpenCV: {cv.__version__} ')
-    print(f'Matplotlib: {plt.__version__} ')
+    print("")
+    print("Pachage")
+    print(f"PyTorch: {torch.__version__} ")
+    print(f"TensorFlow: {tf.__version__} ")
+    print(f"Pandas: {pd.__version__} ")
+    print(f"OpenCV: {cv.__version__} ")
+    print(f"Matplotlib: {plt.__version__} ")
