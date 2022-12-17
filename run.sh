582d58ef25317754a813dcf98b4094057bedaf06
Asuke <62407988+aaout@users.noreply.github.com>
2022-12-18 00:16:25 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ if [ "${IMAGE_NAME}" = "" ]; then
 exit 1
 fi
 
-docker run --rm -it --ipc=host --shm-size=20g --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -v $(pwd):/usr/local/src/${IMAGE_NAME} -w /usr/local/src/${IMAGE_NAME} ${IMAGE_NAME} bash
+docker run --rm -it --ipc=host --shm-size=20g --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -v $(pwd):/workspaces -w /workspaces ${IMAGE_NAME} bash
